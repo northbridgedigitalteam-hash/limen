5db1c5886956a9898bc03435794246b3865deda6
northbridgedigitalteam-hash <northbridgedigitalteam@gmail.com>
2026-01-06 11:22:34 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,14 +3,7 @@ const OFFLINE_URL = "/index.html";
 
 self.addEventListener("install", async event=>{
   const cache = await caches.open(CACHE_NAME);
-  await cache.addAll([
-    OFFLINE_URL,
-    "/style.css",
-    "/app.js",
-    "/interventions.js",
-    "/icon-192.png",
-    "/icon-512.png"
-  ]);
+  await cache.addAll([OFFLINE_URL,"/style.css","/app.js","/interventions.js","/icon-192.png","/icon-512.png"]);
   self.skipWaiting();
 });
 
@@ -18,10 +11,6 @@ self.addEventListener("activate", event=>{ event.waitUntil(clients.claim()); });
 
 self.addEventListener("fetch", event=>{
   if(event.request.mode==="navigate"){
-    event.respondWith(caches.open(CACHE_NAME).then(async cache=>{
-      try{ return await fetch(event.request); } catch(err){ return await cache.match(OFFLINE_URL); }
-    }));
-  } else {
-    event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
-  }
+    event.respondWith(caches.open(CACHE_NAME).then(async cache=>{ try{return await fetch(event.request);} catch(err){return await cache.match(OFFLINE_URL);} }));
+  } else { event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request))); }
 });
